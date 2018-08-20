d87767754da2d802b4a892ec7ab6e8a1dba9ce98
Vinicius A. L. Souza <vinicius.060200@gmail.com>
2018-08-20 11:54:47 -0300

    Resize function fixed

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,36 +1,29 @@
 "use strict";
 
-// Obtain a reference to the canvas element using its id.
-var htmlCanvas = document.getElementById("myCanvas");
-// Obtain a graphics context on the canvas element for drawing.
-var ctx = htmlCanvas.getContext('2d');
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext('2d');
 
 $(function(){
-    // Start listening to resize events and draw canvas.
     initialize();
 });
 
 function initialize() {
-    // Register an event listener to call the resizeCanvas() function 
-    // each time the window is resized.
-    window.addEventListener('resize', resizeCanvas, false);
-    // Draw canvas border for the first time.
-    resizeCanvas();
+    window.addEventListener('resize', drawCanvas, false);
+    drawCanvas();
 }
 
-// Runs each time the DOM window resize event fires.
-// Resets the canvas dimensions to match window,
-// then draws the new borders accordingly.
-function resizeCanvas() {
-    htmlCanvas.width = window.innerWidth;
-    htmlCanvas.height = window.innerHeight;
-    redraw();
+function drawCanvas() {
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+
+    drawBackground();
 }
 
-// Display custom canvas. In this case it's a blue, 5 pixel 
-// border that resizes along with the browser window.
-function redraw() {
-    ctx.fillStyle = "#00293e";
+function drawBackground() {
+    var grd = ctx.createRadialGradient(c.width / 2, c.height / 2, c.width / 5, c.width / 2, c.height / 2, c.width / 1.1);
+    grd.addColorStop(0, "#001433");
+    grd.addColorStop(1, "#425979");
+
+    ctx.fillStyle = grd;
     ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
-    // context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
 }
