aa13f1b0f2dcffdac21d8986d8b7a108a4691fa9
Vinicius A. L. Souza <vinicius.060200@gmail.com>
2019-05-03 19:49:23 -0400

    Snake made into a linked list

--- javaScript/snake.js
+++ javaScript/snake.js
@@ -1,27 +1,42 @@
-function snake(){
-    this.x = floor(floor(width/resolution) / 2 - 1) * resolution;
-    this.y = floor(floor(height/resolution) / 2 - 1) * resolution;
+function snakeBody(xVal, yVal){
+    this.x = xVal;
+    this.y = yVal;
+    this.next = null;
+}
+
+function snake() {
+    let x = floor(floor(width/resolution) / 2 - 1) * resolution;
+    let y = floor(floor(height/resolution) / 2 - 1) * resolution;
+
+    let head = new snakeBody(x, y);
+
     this.xSpeed = 0;
     this.ySpeed = 0;
-    this.tail = [];
+
+    this.size = 1;
 
     this.update = function() {
-        this.x = this.x + this.xSpeed*resolution;
-        this.y = this.y + this.ySpeed*resolution;
+        head.x = head.x + this.xSpeed*resolution;
+        head.x = head.y + this.ySpeed*resolution;
 
-        if(this.x >= width) this.x = 0;
-        if(this.x < 0) this.x = width;
-        if(this.y >= height) this.y = 0;
-        if(this.y < 0) this.y = height;
+        if(head.x >= width) head.x = 0;
+        if(head.x < 0) head.x = width;
+        if(head.y >= height) head.y = 0;
+        if(head.y < 0) head.y = height;
     }
 
     this.show = function(){
         fill(255);
-        rect(this.x, this.y, resolution, resolution);
+        rect(head.x, head.y, resolution, resolution);
     }
 
     this.eat = function(foodPos){
-        if(this.x === foodPos.x && this.y === foodPos.y){
+        if(head.x === foodPos.x && this.y === foodPos.y){
+            this.size++;
+            // this.tail = new snake();
+            // delete this.tail.xSpeed;
+            // delete this.tail.ySpeed;
+
             return true;
         }
         return false;
